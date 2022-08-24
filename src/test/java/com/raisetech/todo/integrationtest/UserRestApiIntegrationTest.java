91905705819802ca91fdb8fdbe3b5d2b1c355725
mkmmr <k.vanilla.0830@gmail.com>
2022-08-24 23:08:21 +0900

    UserRestApiIntegrationTest.javaでZonedDateTime.now()の返す値を固定化

--- src/test/java/com/raisetech/todo/integrationtest/UserRestApiIntegrationTest.java
+++ src/test/java/com/raisetech/todo/integrationtest/UserRestApiIntegrationTest.java
@@ -3,10 +3,10 @@ package com.raisetech.todo.integrationtest;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.Customization;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
-import org.skyscreamer.jsonassert.comparator.CustomComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,6 +19,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
@@ -30,6 +32,8 @@ public class UserRestApiIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    ZonedDateTime zonedDateTime = ZonedDateTime.of(2022, 8, 24, 0, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
 
     @Test
     @DataSet(value = "datasets/to_do_list.yml")
@@ -81,21 +85,23 @@ public class UserRestApiIntegrationTest {
     @DataSet(value = "datasets/to_do_list.yml")
     @Transactional
     void 存在しないタスクIDを指定した時にNotFoundが返ってくること() throws Exception{
-        String response = mockMvc.perform(MockMvcRequestBuilders.get("/todos/99"))
-                .andExpect(MockMvcResultMatchers.status().isNotFound())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        try(MockedStatic<ZonedDateTime> zonedDateTimeMockedStatic = Mockito.mockStatic(ZonedDateTime.class)) {
+            zonedDateTimeMockedStatic.when(ZonedDateTime::now).thenReturn(zonedDateTime);
 
-        String expected = "{" +
-                "    \"error\": \"Not Found\"," +
-                "    \"status\": \"404\"," +
-                "    \"timestamp\": \"\"," +
-                "    \"path\": \"/todos/99\"," +
-                "    \"message\": \"タスクが存在しません\"" +
-                "}";
+            String response = mockMvc.perform(MockMvcRequestBuilders.get("/todos/99"))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals(expected, response,
-                new CustomComparator(JSONCompareMode.STRICT,
-                        new Customization("timestamp", ((o1, o2) -> true))));
+            String expected = "{" +
+                    "    \"error\": \"Not Found\"," +
+                    "    \"status\": \"404\"," +
+                    "    \"timestamp\": \"2022-08-24T00:00+09:00[Asia/Tokyo]\"," +
+                    "    \"path\": \"/todos/99\"," +
+                    "    \"message\": \"タスクが存在しません\"" +
+                    "}";
+
+            JSONAssert.assertEquals(expected, response, JSONCompareMode.STRICT);
+        }
     }
 
     @Test
@@ -126,55 +132,59 @@ public class UserRestApiIntegrationTest {
     @DataSet(value = "datasets/to_do_list.yml")
     @Transactional
     void nullをPOSTした時にBadRequestが返ってくること() throws Exception{
-        String response = mockMvc
-                .perform(MockMvcRequestBuilders
-                        .post("/todos")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}")
-                        .header(HttpHeaders.ACCEPT_LANGUAGE, "ja-JP"))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        try(MockedStatic<ZonedDateTime> zonedDateTimeMockedStatic = Mockito.mockStatic(ZonedDateTime.class)) {
+            zonedDateTimeMockedStatic.when(ZonedDateTime::now).thenReturn(zonedDateTime);
 
-        String expected = "{" +
-                "    \"error\": \"Bad Request\"," +
-                "    \"status\": \"400\"," +
-                "    \"timestamp\": \"\"," +
-                "    \"message\": {" +
-                "        \"task\": \"空白は許可されていません\"," +
-                "        \"limitDate\": \"null は許可されていません\"" +
-                "    }" +
-                "}";
+            String response = mockMvc
+                    .perform(MockMvcRequestBuilders
+                            .post("/todos")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{}")
+                            .header(HttpHeaders.ACCEPT_LANGUAGE, "ja-JP"))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals(expected, response,
-                new CustomComparator(JSONCompareMode.STRICT,
-                        new Customization("timestamp", ((o1, o2) -> true))));
+            String expected = "{" +
+                    "    \"error\": \"Bad Request\"," +
+                    "    \"status\": \"400\"," +
+                    "    \"timestamp\": \"2022-08-24T00:00+09:00[Asia/Tokyo]\"," +
+                    "    \"message\": {" +
+                    "        \"task\": \"空白は許可されていません\"," +
+                    "        \"limitDate\": \"null は許可されていません\"" +
+                    "    }" +
+                    "}";
+
+            JSONAssert.assertEquals(expected, response, JSONCompareMode.STRICT);
+        }
     }
 
     @Test
     @DataSet(value = "datasets/to_do_list.yml")
     @Transactional
     void limitDateに有効な型以外をPOSTした時にBadRequestが返ってくること() throws Exception{
-        String response = mockMvc
-                .perform(MockMvcRequestBuilders
-                        .post("/todos")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{" +
-                                "    \"task\": \"Updateの実装\"," +
-                                "    \"limitDate\": \"2022-8-25\"" +
-                                "}"))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        try(MockedStatic<ZonedDateTime> zonedDateTimeMockedStatic = Mockito.mockStatic(ZonedDateTime.class)) {
+            zonedDateTimeMockedStatic.when(ZonedDateTime::now).thenReturn(zonedDateTime);
 
-        String expected = "{" +
-                "    \"error\": \"Bad Request\"," +
-                "    \"status\": \"400\"," +
-                "    \"timestamp\": \"\"," +
-                "    \"message\": \"2022-8-25は有効ではありません。limitDateは'yyyy-MM-dd'形式で入力してください。\"" +
-                "}";
+            String response = mockMvc
+                    .perform(MockMvcRequestBuilders
+                            .post("/todos")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{" +
+                                    "    \"task\": \"Updateの実装\"," +
+                                    "    \"limitDate\": \"2022-8-25\"" +
+                                    "}"))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals(expected, response,
-                new CustomComparator(JSONCompareMode.STRICT,
-                        new Customization("timestamp", ((o1, o2) -> true))));
+            String expected = "{" +
+                    "    \"error\": \"Bad Request\"," +
+                    "    \"status\": \"400\"," +
+                    "    \"timestamp\": \"2022-08-24T00:00+09:00[Asia/Tokyo]\"," +
+                    "    \"message\": \"2022-8-25は有効ではありません。limitDateは'yyyy-MM-dd'形式で入力してください。\"" +
+                    "}";
+
+            JSONAssert.assertEquals(expected, response, JSONCompareMode.STRICT);
+        }
     }
 
     @Test
@@ -204,81 +214,87 @@ public class UserRestApiIntegrationTest {
     @DataSet(value = "datasets/to_do_list.yml")
     @Transactional
     void nullにタスク変更しようとした時にBadRequestが返ってくること() throws Exception{
-        String response = mockMvc
-                .perform(MockMvcRequestBuilders
-                        .patch("/todos/3")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}")
-                        .header(HttpHeaders.ACCEPT_LANGUAGE, "ja-JP"))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        try(MockedStatic<ZonedDateTime> zonedDateTimeMockedStatic = Mockito.mockStatic(ZonedDateTime.class)) {
+            zonedDateTimeMockedStatic.when(ZonedDateTime::now).thenReturn(zonedDateTime);
 
-        String expected = "{" +
-                "    \"error\": \"Bad Request\"," +
-                "    \"status\": \"400\"," +
-                "    \"timestamp\": \"\"," +
-                "    \"message\": {" +
-                "        \"task\": \"空白は許可されていません\"," +
-                "        \"limitDate\": \"空白は許可されていません\"" +
-                "    }" +
-                "}";
+            String response = mockMvc
+                    .perform(MockMvcRequestBuilders
+                            .patch("/todos/3")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{}")
+                            .header(HttpHeaders.ACCEPT_LANGUAGE, "ja-JP"))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals(expected, response,
-                new CustomComparator(JSONCompareMode.STRICT,
-                        new Customization("timestamp", ((o1, o2) -> true))));
+            String expected = "{" +
+                    "    \"error\": \"Bad Request\"," +
+                    "    \"status\": \"400\"," +
+                    "    \"timestamp\": \"2022-08-24T00:00+09:00[Asia/Tokyo]\"," +
+                    "    \"message\": {" +
+                    "        \"task\": \"空白は許可されていません\"," +
+                    "        \"limitDate\": \"空白は許可されていません\"" +
+                    "    }" +
+                    "}";
+
+            JSONAssert.assertEquals(expected, response, JSONCompareMode.STRICT);
+        }
     }
 
     @Test
     @DataSet(value = "datasets/to_do_list.yml")
     @Transactional
     void limitDateに有効な型以外に変更しようとした時にBadRequestが返ってくること() throws Exception{
-        String response = mockMvc
-                .perform(MockMvcRequestBuilders
-                        .patch("/todos/3")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{" +
-                                "    \"task\": \"Updateの実装\"," +
-                                "    \"limitDate\": \"aaaa\"" +
-                                "}"))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        try(MockedStatic<ZonedDateTime> zonedDateTimeMockedStatic = Mockito.mockStatic(ZonedDateTime.class)) {
+            zonedDateTimeMockedStatic.when(ZonedDateTime::now).thenReturn(zonedDateTime);
 
-        String expected = "{" +
-                "    \"error\": \"Bad Request\"," +
-                "    \"status\": \"400\"," +
-                "    \"timestamp\": \"\"," +
-                "    \"message\": \"aaaaは有効ではありません。limitDateは'yyyy-MM-dd'形式で入力してください。\"" +
-                "}";
+            String response = mockMvc
+                    .perform(MockMvcRequestBuilders
+                            .patch("/todos/3")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{" +
+                                    "    \"task\": \"Updateの実装\"," +
+                                    "    \"limitDate\": \"aaaa\"" +
+                                    "}"))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals(expected, response,
-                new CustomComparator(JSONCompareMode.STRICT,
-                        new Customization("timestamp", ((o1, o2) -> true))));
+            String expected = "{" +
+                    "    \"error\": \"Bad Request\"," +
+                    "    \"status\": \"400\"," +
+                    "    \"timestamp\": \"2022-08-24T00:00+09:00[Asia/Tokyo]\"," +
+                    "    \"message\": \"aaaaは有効ではありません。limitDateは'yyyy-MM-dd'形式で入力してください。\"" +
+                    "}";
+
+            JSONAssert.assertEquals(expected, response, JSONCompareMode.STRICT);
+        }
     }
 
     @Test
     @DataSet(value = "datasets/to_do_list.yml")
     @Transactional
     void 存在しないタスクIDを変更しようとした時にNotFoundが返ってくること() throws Exception{
-        String response = mockMvc.perform(MockMvcRequestBuilders
-                        .patch("/todos/99")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{" +
-                                "    \"task\": \"Updateの実装\"," +
-                                "    \"limitDate\": \"2022-08-25\"" +
-                                "}"))
-                .andExpect(MockMvcResultMatchers.status().isNotFound())
-                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        try(MockedStatic<ZonedDateTime> zonedDateTimeMockedStatic = Mockito.mockStatic(ZonedDateTime.class)) {
+            zonedDateTimeMockedStatic.when(ZonedDateTime::now).thenReturn(zonedDateTime);
 
-        String expected = "{" +
-                "    \"error\": \"Not Found\"," +
-                "    \"status\": \"404\"," +
-                "    \"timestamp\": \"\"," +
-                "    \"path\": \"/todos/99\"," +
-                "    \"message\": \"タスク (id = 99) は存在しません\"" +
-                "}";
+            String response = mockMvc.perform(MockMvcRequestBuilders
+                            .patch("/todos/99")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{" +
+                                    "    \"task\": \"Updateの実装\"," +
+                                    "    \"limitDate\": \"2022-08-25\"" +
+                                    "}"))
+                    .andExpect(MockMvcResultMatchers.status().isNotFound())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
-        JSONAssert.assertEquals(expected, response,
-                new CustomComparator(JSONCompareMode.STRICT,
-                        new Customization("timestamp", ((o1, o2) -> true))));
+            String expected = "{" +
+                    "    \"error\": \"Not Found\"," +
+                    "    \"status\": \"404\"," +
+                    "    \"timestamp\": \"2022-08-24T00:00+09:00[Asia/Tokyo]\"," +
+                    "    \"path\": \"/todos/99\"," +
+                    "    \"message\": \"タスク (id = 99) は存在しません\"" +
+                    "}";
+
+            JSONAssert.assertEquals(expected, response, JSONCompareMode.STRICT);
+        }
     }
 }
