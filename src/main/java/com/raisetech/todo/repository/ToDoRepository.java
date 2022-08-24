355ffc81fd758756bcfe650943afc63ff720f707
mkmmr <k.vanilla.0830@gmail.com>
2022-08-24 21:11:58 +0900

    SQLを大文字に統一

--- src/main/java/com/raisetech/todo/repository/ToDoRepository.java
+++ src/main/java/com/raisetech/todo/repository/ToDoRepository.java
@@ -9,14 +9,14 @@ import java.util.Optional;
 @Mapper
 public interface ToDoRepository {
 
-    @Select("SELECT * From to_do_list")
+    @Select("SELECT * FROM to_do_list")
     List<ToDoRecord> findAllTask();
 
     @Select("SELECT * FROM to_do_list WHERE id = #{id}")
     Optional<ToDoRecord> findById(int id);
 
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    @Insert("INSERT INTO to_do_list (is_done, task, limit_date) values (#{done}, #{task}, #{limitDate})")
+    @Insert("INSERT INTO to_do_list (is_done, task, limit_date) VALUES (#{done}, #{task}, #{limitDate})")
     void insert(ToDoRecord toDoRecord);
 
     @Update("UPDATE to_do_list SET task = #{task}, limit_date = #{limitDate} WHERE id = #{id}")
